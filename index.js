301ac8165ef68d4a6f1ba0a4cc7d67c98ae14be6
eshaidrees <eshaidrees190@gmail.com>
2024-06-29 12:09:48 -0700

    Update

---
+++ index.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import { differenceInSeconds } from "date-fns";
+import inquirer from "inquirer";
+const res = await inquirer.prompt({
+    name: "userInput",
+    type: "number",
+    message: "Please enter the amount of second",
+    validate: (input => {
+        if (isNaN(input)) {
+            return "Please enter valid number";
+        }
+        else if (input > 60) {
+            return "Seconds must be in 60";
+        }
+        else {
+            return true;
+        }
+    })
+});
+let input = res.userInput;
+function startTime(val) {
+    const intiTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(intiTime);
+    setInterval(() => {
+        const currentTiime = new Date();
+        const timeDiff = differenceInSeconds(intervalTime, currentTiime);
+        if (timeDiff <= 0) {
+            console.log("Timer has expired");
+            process.exit();
+        }
+        const min = Math.floor((timeDiff % (3500 * 24)) / 3600);
+        const sec = Math.floor(timeDiff % 60);
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}  `);
+    }, 1000);
+}
+startTime(input);
